141b82237e31b8525d646a86a53ae830117d2486
Mohamad Mohamad <mmohamad115@student.novacollege.nl>
2023-06-07 10:55:50 +0200

    Het structuur van de navbar gemaakt

---
+++ site/database.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuratie
+$host  = "mariadb";
+$dbuser = "root";
+$dbpass = "password";
+$dbname = "projectblok4";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
---
+++ site/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/header.css">
+    <title>navbar</title>
+</head>
+
+<body>
+    <nav>
+        <div class="menu">
+            <div class="logo"><a href="index.php"><img src="img/logo.jpg"></a></div>
+            <ul>
+                <li><a href="#">WORKOUTS</a></li>
+                <li><a href="#">PROGRAMS</a></li>
+                <li><a href="#">CHALLENGES</a></li>
+                <li><a href="#">GUIDES</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
